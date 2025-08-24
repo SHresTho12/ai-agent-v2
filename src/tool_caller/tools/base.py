13839eb05fe17c56b43c8ea79391c403b94c4c9c
shrestho12 <talimul12bari@gmail.com>
2025-08-24 09:59:25 +0600

    Update: Wrapper function for the better logging and error handling of tool execution function

--- src/tool_caller/tools/base.py
+++ src/tool_caller/tools/base.py
@@ -1,6 +1,16 @@
+
+import time
+import logging
+
 from abc import ABC, abstractmethod
 from typing import Dict, Any, List, Optional
 from pydantic import BaseModel,ValidationError
+
+
+
+
+
+logger = logging.getLogger(__name__)
 
 
 
@@ -45,20 +55,52 @@ class BaseTool(ABC):
     def schema(self) -> ToolSchema:
         """Tool schema for LLM"""
         pass
-    
+   
+   
     @abstractmethod
-    async def execute(self, **kwargs) -> ToolResponse:
+    async def _run(self, **kwargs) -> Any:
         """
-        Execute the tool with given parameters.
+        Core logic of the tool. Must be implemented by subclasses.
         
         Args:
             kwargs: Arbitrary keyword arguments matching tool schema.
 
         Returns:
-            ToolResponse: Standardized success/error response.
+            Any: Result of the tool execution.
         """
         pass
-    
+    @abstractmethod
+    async def execute(self, **kwargs) -> ToolResponse:
+        """
+        Execute the tool with given parameters.
+
+        Args:
+            kwargs: Arbitrary keyword arguments matching tool schema.
+
+        Returns:
+            ToolResponse: Standardized success/error response.
+
+        """
+        start_time = time.time()
+        try:
+            logger.debug(f"Executing tool {self.name} with params: {kwargs}")
+            if self.parameters_model:
+                try:
+                    kwargs = self.parameters_model(**kwargs).dict()
+                except ValidationError as ve:
+                    logger.error(f"{self.name} Parameter validation error: {ve}")
+                    return ToolResponse(success=False, error=f"Parameter validation error: {ve}", execution_time=time.time() - start_time)
+            
+            result = await self._run(**kwargs)
+            execution_time = time.time() - start_time
+            logger.info(f"Tool {self.name} executed successfully in {execution_time:.4f}s")
+            
+            return ToolResponse(success=True, result=result, execution_time=execution_time)
+        except Exception as e:
+            execution_time = time.time() - start_time
+            logger.exception(f"Tool {self.name} execution failed: {e} after {execution_time:.4f}s")
+            return ToolResponse(success=False, error=str(e), execution_time=execution_time)
+
     @property
     def parameters_model(self) -> Optional[BaseModel]:
         """
